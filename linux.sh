fb39d59ed5ae9dff72964d44ce1c37905f575f34
Phoenix <158185295+ZombieGeeK0@users.noreply.github.com>
2024-05-23 11:45:47 +0200

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,32 +1,32 @@
 function install() {
    clear
-   echo -e "\033[32m[~] Actualizando paquetes..."
+   echo -e "\033[32m[~] Updating packets..."
    sudo apt update -y && sudo apt upgrade -y
   
 
    sleep 3
    which python3 > /dev/null 2>&1
    if [ "$?" -eq "0" ]; then
-   echo -e "\033[32m\n[~] Python3 ya esta instalado."
+   echo -e "\033[32m\n[~] python3 is installed"
    else
-   echo -e "\033[31m\n[!] Python3 no esta instalado."
+   echo -e "\033[31m\n[!] python3 isn't installed"
    sleep 2
-   echo -e "\033[32m\n[~] Instalando python3..."
+   echo -e "\033[32m\n[~] Installing python3..."
    sudo apt install python3 -y
    fi
    
    sleep 3
    which pip3 > /dev/null 2>&1
    if [ "$?" -eq "0" ]; then
-   echo -e "\033[32m\n[~] pip3 ya esta instalado."
+   echo -e "\033[32m\n[~] pip3 is installed"
    else
-   echo -e "\033[31m\n[!] pip3 no esta instalado."
+   echo -e "\033[31m\n[!] pip3 isn't installed"
    sleep 2
-   echo -e "\033[32m\n[~] Instalando pip3..."
+   echo -e "\033[32m\n[~] Installing pip3..."
    sudo apt install python3-pip -y
    fi
    
-   echo -e "\033[32m\n[~] Instalando requerimientos..."
+   echo -e "\033[32m\n[~] Installing requirements..."
    pip3 install -r requirements.txt
 }
 
